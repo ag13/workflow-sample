d38602dec5d9f5e6232fe498f87321c5b5cdcf30
Anupam Gore <anupamgore@gmail.com>
2020-09-14 12:50:02 +0530

    Updates

---
+++ precache-manifest.4de862d5e11e0d0d9bfb5665a7f33c4a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8839bdf20b7e2af43debcafba9ef168e",
+    "url": "/workflow-sample/index.html"
+  },
+  {
+    "revision": "b8c670e0c028d4066205",
+    "url": "/workflow-sample/static/css/2.ed0d9eaf.chunk.css"
+  },
+  {
+    "revision": "b23e050ee0840eacb7c1",
+    "url": "/workflow-sample/static/css/main.9662eb34.chunk.css"
+  },
+  {
+    "revision": "b8c670e0c028d4066205",
+    "url": "/workflow-sample/static/js/2.3a16366a.chunk.js"
+  },
+  {
+    "revision": "d09ac505fec0e9130a1d5b85fb3fa6dc",
+    "url": "/workflow-sample/static/js/2.3a16366a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b23e050ee0840eacb7c1",
+    "url": "/workflow-sample/static/js/main.b2662750.chunk.js"
+  },
+  {
+    "revision": "d256cd070d94351220dc",
+    "url": "/workflow-sample/static/js/runtime-main.782fefaf.js"
+  }
+]);
